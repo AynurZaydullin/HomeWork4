28f300f7e91c4fff43b2ba9c44ed50fa3a175ca9
AynurZaydullin <zaydullin.aynur@yandex.ru>
2022-11-17 09:47:04 +0300

    Task 1 and task 2 is completed.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задачи. Условный оператор. Часть 2."
+        System.out.println("Задачи. Условный оператор. Часть 2.");
+        //Задача 1.
+        System.out.println("Задание 1.");
+        int ageOfUser = 19;
+        if(ageOfUser >= 18){
+            System.out.println("Поздравляю вас с совершеннолетием!");
+        }
+        System.out.println("Возраст совершеннолетия еще не наступил, и нужно немного подождать.");
+        System.out.println();
+        //Задача 2.
+        System.out.println("Задача 2.");
+        int peopleAge = 20;
+        if((peopleAge >= 7) && (peopleAge < 18)){
+            System.out.println("Ребёнок ходит в школу.");
+        }
+        if (peopleAge >= 18){
+            System.out.println("Человек уже закончил школу и может отправляться в университет.");
+        }
+        System.out.println();
     }
 }
